0c68905a522fd9891e8813651efa9e76ead30484
OlowDev <saleban.olow@mymail.champlain.edu>
2018-02-22 18:48:43 -0500

    Update Huffman.h

--- Huffman.h
+++ Huffman.h
@@ -1,15 +1,21 @@
-#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <string>
+using namespace std;
 
-struct Node
+struct Tree
 {
-  
+	char characts;
+	int frequentChar;
+	Tree *leftNode, *rightNode;
+
+	bool operator()(Tree* lt, Tree* rt)
+	{
+		return lt->frequentChar > rt->frequentChar;
+	}
 };
 
-class Huffman
-{
- void encode();
- void decode();
- void createHuff();
- 
-  
-}
+Tree* create_node(char, int, Tree*, Tree*);
+void encode_characters(Tree* root, string str, unordered_map<char, string> &huffCode);
+void decode_characters(Tree* root, int &pos, string str);
+void create_huff(string data);
