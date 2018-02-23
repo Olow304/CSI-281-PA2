6d021f44913aa1b3eca22c8136e6f7b03a293802
OlowDev <saleban.olow@mymail.champlain.edu>
2018-02-22 19:02:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "Huffman.h"
-#include <fstream>
-
+using namespace std;
 
 int main()
 {
+	string testing = "Just give some characters to test this program";
+	create_huff(testing);
 
-
-  system("pause");
-  return 0;
+	system("pause");
+	return 0;
 }
