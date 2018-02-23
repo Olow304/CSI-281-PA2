31025e0097826002b8ecc590e9032f848225e54c
Victor Alves <victor.alves@mymail.champlain.edu>
2018-02-23 16:54:08 -0500

    Reorganized program, worked on the first Output function
    Had to change the unordered maps and the member variable to type string, and they worked so much better than if they were type char.

--- functions.cpp
+++ functions.cpp
@@ -2,12 +2,27 @@
 
 using namespace std;
 
-// Alternate way of adding words to the priority queue, char by char. Ignore this.
-void addWord(string wordToEncode)
+void writeHuffmanOutput(ofstream &stream, string word)
 {
-	for (int i = 0; i < wordToEncode.size(); i++)
-	{
-		//create_huff(wordToEncode[i]);
-	}
+	// we need to output this data into file called huffmanoutput.txt
+	cout << "Huffman Code: " << endl;
+	stream << "Huffman Code for the word " << word << ":" << "\n";
 
+	for (auto tree : huffmanSequence)
+	{
+		cout << tree.first << " " << tree.second << endl;
+		stream << "Letter: " << tree.first << "\n";
+		stream << "Sequence: " << tree.second << "\n\n";
+	}
+		
+	cout << endl;
+}
+
+void writeHuffmanDecoded(ofstream &stream, string word)
+{
+	// we need to output this data into file called reconstructedinput.txt
+	for (char c : data)
+		byteString += huffmanSequence[c] + " ";
+	cout << endl;
+	cout << "Encoded huffman code: " << byteString << " " << endl;
 }
